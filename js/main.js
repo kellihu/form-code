fa3da94ef7ced8512166e9e8cecf86315db36fbf
kellihu <46789868+kellihu@users.noreply.github.com>
2022-03-12 13:49:41 -0600

    Halley's Comet

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
+let x = innerWidth / 2
+let y = innerHeight / 2
+let dy = 2
+let dx = 2
+
 function setup() {
   createCanvas(innerWidth, innerHeight)
-  background(0)
-  let x = width / 2
-  let y = height / 2
-  let r = width / 4
-  ellipse(x, y, r)
+  background(255, 0, 0, 100)
+}
+
+function draw () {
+  background(255, 0, 100, 10)
+  let d = width /8
+  let r = d/2
+  x += dx
+  y += dy
+
+if (y > height-r || y<r) dy = -dy
+if (x > width - r || x< r) dx = -dx
+
+  ellipse (x, y, d)
 }
